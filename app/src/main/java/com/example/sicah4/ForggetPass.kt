e080172faddd73b2b59477b922f64e30e56c130f
Beackz <jbanosi1800@alumno.ipn.mx>
2023-07-14 15:08:15 -0600

    N/A

---
+++ app/src/main/java/com/example/sicah4/ForggetPass.kt
@@ -0,0 +1,15 @@
+package com.example.sicah4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.EmptyStackException
+
+class ForggetPass : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgget_pass)
+    }
+
+
+}
--- app/src/main/java/com/example/sicah4/MainActivity.kt
+++ app/src/main/java/com/example/sicah4/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.sicah4
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+      val btnpass = findViewById<Button>(R.id.btnPass)
+        btnpass.setOnClickListener {
+            btnpass()
+        }
+    }
+    private fun btnpass()
+    {
+        val i = Intent(this, ForggetPass::class.java)
+        startActivity(i)
     }
 }
